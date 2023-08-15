0c35fafa6483ef80233bc6164272edb97157129e
root <root@DESKTOP-FN6KIGE>
2023-08-15 18:14:19 -0300

    feat: start model workflow

--- src/__main__.py
+++ src/__main__.py
@@ -1,15 +1,10 @@
 import argparse
 
 import hydra
-import pandas as pd
-from keras.preprocessing.image import ImageDataGenerator
-from keras.utils import image_dataset_from_directory
 from omegaconf import DictConfig
 
-from modelling.model import PoissonAgePredictor
-from preprocessing import Dataset, preprocess
-from preprocessing.generators import create_flow, split_data
-from preprocessing.loading import load_images, make_dataframe, split
+from modelling.model import AgePredictor
+from preprocessing import make_generator, split_train_val_test
 
 if __name__ == '__main__':
     # Load configs
@@ -26,9 +21,37 @@ if __name__ == '__main__':
                         '--test-size',
                         help='Test set size',
                         type=float)
+    parser.add_argument('-b',
+                        '--batch-size',
+                        help='Batch size',
+                        type=int)
+    parser.add_argument('-i',
+                        '--image-size',
+                        help='Image size',
+                        type=int)
+    parser.add_argument('-s',
+                        '--train-val-test-split',
+                        help='Split training, validation and test sets',
+                        action=argparse.BooleanOptionalAction)
     run_config: DictConfig = DictConfig(vars(parser.parse_args()))
 
-    # split(project_config, run_config)
+    split_train_val_test(project_config=project_config,
+                         run_config=run_config,
+                         seed=1)
+
+    train = make_generator(project_config=project_config,
+                           run_config=run_config,
+                           set_='train',
+                           what='age')
+    val = make_generator(project_config=project_config,
+                         run_config=run_config,
+                         set_='val',
+                         what='age')
+
+    model = AgePredictor(input_shape=train.element_spec[0].shape[1:])
+    model.fit(train, val)
+
+    pass
 
     # Load data
     # train_df, val_df, test_df = make_dataframe(
@@ -55,13 +78,13 @@ if __name__ == '__main__':
     # dataset: Dataset = preprocess(project_config=project_config,
     #                               run_config=run_config)
 
-    train = load_images(project_config.data.train)
+    # train = load_images(project_config.data.train)
 
-    model = PoissonAgePredictor(train_set=train,
-                                # val_set=val_set,
-                                # test_set=test_set,
-                                # dataset=dataset,
-                                config=run_config)
-    model.fit()
+    # model = PoissonAgePredictor(train_set=train,
+    #                             # val_set=val_set,
+    #                             # test_set=test_set,
+    #                             # dataset=dataset,
+    #                             config=run_config)
+    # model.fit()
 
     pass
